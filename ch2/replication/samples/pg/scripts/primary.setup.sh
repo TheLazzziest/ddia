f0b74c6b0169dd8fef4264479ee858ead0876a8b
TheLazzzies <mxyakovenko9@gmail.com>
2025-11-03 01:03:42 +0300

    Reconfigure settings. Add settings for pgbouncer. Split writing/reading tests

--- ch2/replication/samples/pg/scripts/primary.setup.sh
+++ ch2/replication/samples/pg/scripts/primary.setup.sh
@@ -9,4 +9,13 @@ echo "Running setup_db.sh: Waiting for postgres to be ready..."
 # We use 'trust' here to simplify the workshop setup.
 echo "host replication all 0.0.0.0/0 trust" >> "$PGDATA/pg_hba.conf"
 
+echo "Enforcing SYNCHRONOUS COMMIT."
+PGPASSWORD="$POSTGRES_PASSWORD" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
+  -- Set the commit level to ON
+  ALTER SYSTEM SET synchronous_commit = on; 
+  
+  SELECT pg_reload_conf();
+EOSQL
+
+
 echo "Setup complete. The entrypoint will now load pagila.sql."
